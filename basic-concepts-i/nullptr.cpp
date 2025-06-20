2f6f6a50dc4e084082b0284e6a8be2ca3ab65228
nicky goodman <nickyggoodman@gmail.com>
2025-06-20 12:33:15 -0400

    adding notes

---
+++ basic-concepts-i/nullptr.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+  int* ptr = NULL;
+  int* ptr2 = nullptr;
+  
+  int number = NULL; // NULL is 0
+  //int n2 = nullptr; // compile error. nullptr not convertible to an int
+
+  //int* ptr3 = true ? 0 : nullptr; // compile error. incopatible types.
+
+}
---
+++ basic-concepts-i/typeconversion.cpp
@@ -0,0 +1,13 @@
+/*
+ * type conversions in C++
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  float myfloat = 1.201;
+  int myint = 2;
+  // prints 2.402
+  cout << myfloat * myint << endl;
+}
